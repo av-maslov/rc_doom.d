6fbde08f28c6bd66474be3135e44f44b0543d161
Alexandr Maslov <alexandr.maslov@hm.com>
2022-10-22 11:20:26 +0300

    1st

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((org-roam-db-location . "/home/al/org-roam/silo/hm/org-roam-hm.db")
+     (org-roam-directory . "/home/al/org-roam/silo/hm"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
